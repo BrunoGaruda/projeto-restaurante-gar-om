2115079d6bf315d2665831e3e4852d89dc7bb356
BrunoAlmeida <bruleorn@gmail.com>
2022-12-07 23:47:48 -0300

    adicionando o text global no app com o text componente

--- app/App.tsx
+++ app/App.tsx
@@ -1,17 +1,15 @@
-import { useFonts } from 'expo-font';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { Text } from './src/components/Text';
-
+import { useFonts } from "expo-font";
+import { Main } from "./src/Main";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   //Record = objeto
   // forma de evitar erros ao recarregar, retornando um null.
   // Caso queira usar do google fonts: @expo-google-font (https://docs.expo.dev/guides/using-custom-fonts/)
   const [isFontsLoaded] = useFonts({
-    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
-    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
-    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf')
+    "GeneralSans-400": require("./src/assets/fonts/GeneralSans-Regular.otf"),
+    "GeneralSans-600": require("./src/assets/fonts/GeneralSans-Semibold.otf"),
+    "GeneralSans-700": require("./src/assets/fonts/GeneralSans-Bold.otf")
   });
 
   if (!isFontsLoaded) {
@@ -19,19 +17,12 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text size={24} weight="600" opacity={.5} color="#00f">Hello Dev!</Text>
-      <Text size={20} weight="700" opacity={.8} color="#f0f">Hi!</Text>
-      <StatusBar style='auto' />
-    </View >
+
+    <>
+      <StatusBar style="dark" />
+      <Main />
+    </>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
